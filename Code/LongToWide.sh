9ba55108fcfb30e336eb56bffd3c2e5749566fd7
tomszar <tomasgzarzar@gmail.com>
2018-05-19 17:59:51 -0400

    updated longtowide and jupyter notebook

--- Code/LongToWide.sh
+++ Code/LongToWide.sh
@@ -5,6 +5,6 @@ awk '
 NR>1 \
 {snps[$2]; clust[$3]; val[$2,$3]=$6} \
 END {
-	printf "%s", "rsid"; for (i in snps) printf "\t%s", i; print "";
+	printf "%s", "Genomes"; for (i in snps) printf ",%s", i; print "";
 for (j in clust) {printf "%s", j;
-for (i in snps) printf "\t%s", val[i,j]; print ""} }' CleanGenos_strict.frq.strat > CleanGenos_strict_wide.frq.strat
+for (i in snps) printf ",%s", val[i,j]; print ""} }' CleanGenos_strict.frq.strat > CleanGenos_strict_wide.frq.strat
