5318e27b863d2d4eda88369c8bc8ee6f5d31e6f0
tomszar <tomasgzarzar@gmail.com>
2018-05-21 12:45:47 -0400

    updated code and awk code

--- Code/LongToWide.sh
+++ Code/LongToWide.sh
@@ -3,8 +3,8 @@
 cd ../DataBases/Genotypes
 awk '
 NR>1 \
-{snps[$2]; clust[$3]; val[$2,$3]=$6} \
+{snps[$2]; clust[$3]; a1[$2]=$4; a2[$2]=$5; val[$2,$3]=$7; total[$3]=$8} \
 END {
-	printf "%s", "Genomes"; for (i in snps) printf ",%s", i; print "";
-for (j in clust) {printf "%s", j;
-for (i in snps) printf ",%s", val[i,j]; print ""} }' CleanGenos_strict.frq.strat > CleanGenos_strict_wide.frq.strat
+	printf "%s,%s", "Pop", "Genomes"; for (i in snps) printf ",%s.%s,%s.%s", i,a1[i], i,a2[i]; print "";
+for (j in clust) {printf "%s,%s", j, total[j];
+for (i in snps) printf ",%s,%s", val[i,j], total[j] - val[i,j]; print ""} }' CleanGenos_strict.frq.strat > CleanGenos_strict_wide.frq.strat
